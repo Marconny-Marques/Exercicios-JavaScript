db650aee93dddda2e32a12a61225a424d2d061f9
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2023-10-16 17:52:51 -0300

    estudos dia 16

---
+++ exercicios/script.js
@@ -0,0 +1,21 @@
+let res = window.document.getElementById('res')
+
+function Acao1() {
+    let res = document.querySelector('section#res')
+    res.innerHTML = (`Clicou no primeiro botão`)
+}
+
+function Acao2() {
+    let res = document.querySelector('section#res')
+    res.innerHTML = (`Clicou no segundo botão`)
+}
+
+function Acao3() {
+    let res = document.querySelector('section#res')
+    res.innerHTML = (`Clicou no terceiro botão`)
+}
+
+function Acao4() {
+    let res = document.querySelector('section#res')
+    res.innerHTML = (`Clicou no quarto botão`)
+}
